aa0317175a544556d1f7cd8d2dee09d8ca756f85
shoonia <alexanderz@wix.com>
2021-04-18 19:40:37 +0300

    Build of library

--- dist/esm.js
+++ dist/esm.js
@@ -61,21 +61,28 @@ const createScope = (getData) => (event) => {
 const useScope = (event) => {
   const ctx = event.context;
   const find = (i) => i._id === ctx.itemId;
+
   let repeter = event.target;
+
   // @ts-ignore
   while ((repeter = repeter.parent).type !== '$w.Repeater') {
     /**/
   }
+
   return {
     repeter,
+
     // @ts-ignore
     $item: $w.at(ctx),
+
     get itemData() {
       return repeter.data.find(find);
     },
+
     get index() {
       return repeter.data.findIndex(find);
     },
+
     get data() {
       return repeter.data;
     },
