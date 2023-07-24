e7859882538ccf44426a5e877e8e59e8d775a701
shoonia <alexanderz@wix.com>
2023-07-24 20:01:48 +0300

    Remove comments

--- dist/index.cjs
+++ dist/index.cjs
@@ -18,14 +18,11 @@ const createScope = getData => event => {
 };
 
 const getRepeater = event => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   let node = event.target;
   if (event.context.type !== 'COMPONENT_SCOPE') {
     console.error(`repeater-scope: "#${node.id}" outside of any $w.Repeater context`);
   }
-  while ((node = node.parent).type !== '$w.Repeater') {
-    /**/
-  }
+  while ((node = node.parent).type !== '$w.Repeater') {}
   return node;
 };
 
--- rollup.config.js
+++ rollup.config.js
@@ -31,6 +31,7 @@ export default {
       presets: [
         '@babel/typescript',
       ],
+      comments: false,
     })
   ]
 };
